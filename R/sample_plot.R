64d53154f05f5fc1d610c7d901c9b969dbfdab52
jundoll <jundoll33@gmail.com>
2015-08-14 16:44:59 +0900

    ooooops

--- R/sample_plot.R
+++ R/sample_plot.R
@@ -11,7 +11,7 @@ sample_plot <- function(dat, col = 4, pch = 16, ...) {
   info <- attr(dat, "sample_info")
   xrange <- (info$xrange - mean(info$xrange)) * 2 + mean(info$xrange)
   yrange <- (info$yrange - mean(info$yrange)) * 2 + mean(info$yrange)
-  plot(samples, xlim = xrange, ylim = yrange, type = 'n', asp = TRUE, xlab = "", ylab = "", bty = 'n', xaxt = "n", yaxt = "n", main = info$title)
+  plot(dat, xlim = xrange, ylim = yrange, type = 'n', asp = TRUE, xlab = "", ylab = "", bty = 'n', xaxt = "n", yaxt = "n", main = info$title)
   rect(xrange[1], yrange[1], xrange[2], yrange[2], col = 'orange', border = 'white')
   points(dat, col = col, pch = pch, ...)
 }
